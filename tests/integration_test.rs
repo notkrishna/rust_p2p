c4d551951c2a72b1c11f55d2939ce67783e4df2e
Krishna <krishnapar2ek@gmail.com>
2024-08-03 19:05:00 +0530

    added recieve message test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -25,7 +25,7 @@ async fn test_add_peer(){
 #[tokio::test]
 async fn test_send_message() {
     let broadcast_addr: SocketAddr = "127.0.0.1:6200".parse().unwrap();
-    let peer_addr: SocketAddr = "127.0.0.1:6201".parse().unwrap();
+    let peer_addr: SocketAddr = "127.0.0.1:6203".parse().unwrap();
     
     let node = Node::new(broadcast_addr);
 
@@ -69,4 +69,56 @@ async fn test_send_message() {
         Ok(_) => println!("Message handling completed within time range"),
         Err(_) => panic!("Timeout while waiting for incoming messages"),
     };
+}
+
+#[tokio::test]
+async fn test_recieve_message() {
+
+    let broadcast_addr: SocketAddr = "127.0.0.1:6200".parse().unwrap();
+    let peer_addr: SocketAddr = "127.0.0.1:6205".parse().unwrap();
+    
+    let node = Node::new(broadcast_addr);
+
+    node.add_peer(peer_addr).await;
+
+    let peers= node.get_peers().await;
+    
+    let listener = TcpListener::bind(peer_addr).await.unwrap();
+
+    let node_id = node.id.clone();
+
+    let handle_incoming_connections = tokio::spawn(async move {
+        let (mut socket, _) = listener.accept().await.unwrap();
+        let mut buff = vec![0;1024];
+        let x = socket.read(&mut buff).await.unwrap();
+
+        let msg_recieved: Result<Message, _> = serde_json::from_slice(&buff[..x]);
+        match msg_recieved {
+            Ok(Message::Text {from, content}) => {
+                //test condition
+
+                assert_eq!(from, node_id);
+                assert_eq!(content, "Message recieved!");
+            }
+            _ => panic!("Error deserializing message :("),
+        }
+    });
+
+    //slow bastard needs 5 seconds to start
+    tokio::time::sleep(Duration::from_secs(5)).await;
+
+
+    let msg = Message::Text { 
+        from: node.id.clone(), 
+        content: "Message recieved!".to_string(), 
+    };
+
+    let mut stream = TcpStream::connect(peer_addr).await.unwrap();
+    let json = serde_json::to_string(&msg).unwrap();
+    stream.write_all(json.as_bytes()).await.unwrap();
+
+    match timeout(Duration::from_secs(5), handle_incoming_connections).await {
+        Ok(_) => println!("Successful!"),
+        Err(_) => panic!("Timeout while waiting for incoming messages"),
+    }
 }
