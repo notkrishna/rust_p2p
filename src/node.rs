a87976d9e2ce7fe0f20777524ea0863037688023
Krishna <krishnapar2ek@gmail.com>
2024-08-03 12:57:00 +0530

    adding send message function

--- src/node.rs
+++ src/node.rs
@@ -1,10 +1,15 @@
 
-use std::net::SocketAddr;
+use std::net::{SocketAddr};
 use std::sync::Arc;
 
 use serde::{Deserialize, Serialize};
+use serde_json;
+
 use uuid::Uuid;
-use tokio::sync::Mutex;
+use tokio::sync::{Mutex, mpsc};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
 use log::{
     info,
     warn,
@@ -53,5 +58,44 @@ impl Node {
     pub async fn get_peers(&self) -> Vec<SocketAddr> {
         let peers = self.peers.lock().await;
         peers.clone()
+    }
+
+    pub async fn send_message(&self, msg: &Message, addr: &SocketAddr) -> Result<(), Box<dyn std::error::Error>> {
+        let json = serde_json::to_string(msg)?;
+        let mut stream = TcpStream::connect(addr).await?;
+        stream.write_all(json.as_bytes()).await?;
+        Ok(())
+    }
+
+    pub async fn broadcast_message(&self, msg: &Message) -> Result<(), Box<dyn std::error::Error>> {
+        let peers = self.peers.lock().await;
+        for peer in peers.iter() {
+            self.send_message(msg, peer).await?;
+        }
+        Ok(())
+    }
+
+    pub async fn recieve_message(&self, addr:&SocketAddr, tx: mpsc::Sender<Message>) -> Result<(), Box<dyn std::error::Error>> {
+        let listener = TcpListener::bind(addr).await?;
+        loop {
+            let (mut stream, peer_addr) = listener.accept().await?;
+            let mut buff = vec![0;1024];
+            let x = stream.read(&mut buff).await?;
+            if let Ok(msg) = serde_json::from_slice(&buff[..x]) {
+
+                match &msg {
+                    Message::Handshake { id } => {
+                        self.add_peer(peer_addr).await;
+                    },
+                    Message::Text { 
+                        from, 
+                        content 
+                    } => {
+                        tx.send(msg).await?;
+                    }
+                }
+            };
+    
+        }
     }
 }
